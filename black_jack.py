e9689328db32ecf3e68e35548cddccf170f675ea
Luke Roy <lkry95@gmail.com>
2020-12-02 09:31:36 -0500

    Add files via upload
    Skeleton of a class based Black Jack game.

---
+++ black_jack.py
@@ -0,0 +1,58 @@
+
+
+
+# Create classes for blackjack program
+
+class Deck:
+
+    def __init__(self):
+        pass
+
+    # A function for creating a deck
+    # The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-9, T, Jack, Queen, King
+    def deck_cards(self):
+
+
+
+class Deal:
+
+    def __init__(self):
+        pass
+
+    # A function for creating player and dealers hands
+    # The player and dealer will get 2 cards initially
+
+    def deal_cards(self, cards):
+
+
+
+class Calculation:
+
+    def __init__(self):
+        pass
+
+    # A function for counting the points
+    # This function will count the points of the player and the dealer
+    # The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
+
+    def point_calc(self, my_cards):
+
+
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def player_hit(self, my_cards, deck):
+
+
+
+class Dealer(Player):  # Dealer class is inherited from Player class.
+
+    def __init__(self):
+        pass
+
+
+
+
